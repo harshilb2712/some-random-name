8da0a9391804a98f069cb8a9ca434de887b2d0d1
harshilb2712 <111215389+harshilb2712@users.noreply.github.com>
2023-12-09 11:42:58 +0530

    Create some.py

---
+++ some.py
@@ -0,0 +1,26 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    if y != 0: return x / y
+    else     : return 'Division not possible'
+    
+def calculator():
+    x = int(input('First: '))
+    y = int(input('Second: '))
+    op = input('Operator: ')
+    if   op == '+': print(add(x, y))
+    elif op == '-': print(sub(x, y))
+    elif op == '*': print(mul(x, y))
+    elif op == '/': print(div(x, y))
+    else          : print('Invalid operator input')
+    
+pi = 3
+l = [12, 4, 8]
+e = 2.7
